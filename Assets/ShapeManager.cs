0d70fea665ab57e09500bc3584392df2d5e5567e
Matt Messmer <staind2523@gmail.com>
2016-02-25 20:42:49 -0800

    Move assets and rename. Also spawn object and make it fall.
    I really don't know how to Unity.

---
+++ Assets/ShapeManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+public class ShapeManager : MonoBehaviour
+{
+    public GameObject startingObject;
+
+    public void Start()
+    {
+        // Initialize shapes
+        if (startingObject == null)
+        {
+            throw new InvalidOperationException("startingObject must be set in editor.");
+        }
+
+        startingObject.transform.position = new Vector2(0, 10);
+        startingObject = Instantiate(startingObject);
+
+        // Begin updates, and repeat every second
+        InvokeRepeating("UpdateShapes", 1f, 1f);
+    }
+
+    private void UpdateShapes()
+    {
+        Vector3 position = startingObject.transform.position;
+        startingObject.transform.position = new Vector3(position.x, position.y - 1, 0);
+        Debug.Log(startingObject.transform.position);
+    }
+}
