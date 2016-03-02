777970d5252722367b2be6e2b76a1b8bc26628a4
Matt Messmer <staind2523@gmail.com>
2016-03-01 22:06:04 -0800

    Set up rotation offsets.
    This is done for shape T but not the others yet. It's worth noting that
    this is a very bad implementation and if this project were likely to
    last more than a couple weeks, I would put more thought into it. But
    since I'm only interested in getting this working and finishing it, this
    is good enough.

--- Assets/ShapeManager.cs
+++ Assets/ShapeManager.cs
@@ -72,7 +72,13 @@ public class ShapeManager : MonoBehaviour
             return;
         }
 
-        activeShape.transform.Rotate(0, 0, 90);
+        Vector3 rotationPoint = GetRotationPoint(activeShape);
+        activeShape.transform.RotateAround(activeShape.transform.position + rotationPoint, new Vector3(0, 0, 1), 90);
+        activeShapeRotationIndex++;
+        if (activeShapeRotationIndex == NumRotations)
+        {
+            activeShapeRotationIndex = 0;
+        }
     }
 
     private void Start()
@@ -106,6 +112,7 @@ public class ShapeManager : MonoBehaviour
                 // Collision detected
                 activeShape.transform.position += new Vector3(0, 1);
                 activeShape = null;
+                activeShapeRotationIndex = 0;
                 SpawnNextShape();
             }
         }
@@ -121,6 +128,110 @@ public class ShapeManager : MonoBehaviour
         GameObject shape = (GameObject)Resources.Load("I");
         shape.transform.position = new Vector2(0, 20);
         activeShape = Instantiate(shape);
+    }
+
+    private Vector3 GetRotationPoint(GameObject shape)
+    {
+        // Wow, there's really got to be a better way to figure out which prefab this is
+        // Maybe attach a script with an enum?
+        if (shape.name.StartsWith("T"))
+        {
+            switch (activeShapeRotationIndex)
+            {
+                case 0:
+                    return new Vector3(1.5f, 1.5f);
+                case 1:
+                    return new Vector3(-1.5f, 1.5f);
+                case 2:
+                    return new Vector3(-1.5f, -1.5f);
+                case 3:
+                    return new Vector3(1.5f, -1.5f);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("I"))
+        {
+            switch (activeShapeRotationIndex)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("J"))
+        {
+            switch ((int)shape.transform.rotation.z)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("L"))
+        {
+            switch ((int)shape.transform.rotation.z)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("O"))
+        {
+            switch ((int)shape.transform.rotation.z)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("S"))
+        {
+            switch ((int)shape.transform.rotation.z)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else if (shape.name.StartsWith("Z"))
+        {
+            switch ((int)shape.transform.rotation.z)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return new Vector3(0, 2);
+                default:
+                    throw new InvalidOperationException(string.Format("Unexpected rotation index: {0}", activeShapeRotationIndex));
+            }
+        }
+        else
+        {
+            throw new ArgumentException(string.Format("Unrecognized shape name: {0}", shape.name));
+        }
     }
 
     // Checks all children of shape to make sure they all move to a valid grid location
@@ -157,5 +268,7 @@ public class ShapeManager : MonoBehaviour
     }
 
     private GameObject activeShape;
+    private int activeShapeRotationIndex;
     private GridManager gridManager;
+    private const int NumRotations = 4;
 }
